af8bc97fe2bcb82eaa2249cc50fec3d3ae6d30cb
Parzivand <vdnbezerra@gmail.com>
2025-12-26 20:43:42 +0000

    save

--- CentroDistribuicaoPCO/src/codigo/domain/Expedicao.java
+++ CentroDistribuicaoPCO/src/codigo/domain/Expedicao.java
@@ -1,3 +1,4 @@
+<<<<<<< Updated upstream
 package codigo.domain;
 
 import java.util.HashMap;
@@ -23,3 +24,33 @@ public class Expedicao{
 
     
 }
+=======
+//package codigo.domain;
+//
+//public class Expedicao{
+//
+//    // Atributos
+//
+//    private String estado, Localizacao;
+//    private HashMap<String,Encomenda> encomendas;
+//    // Construtor
+//
+//    public Expedicao(String estado, String localizacao) {
+//        this.estado = estado;
+//        Localizacao = localizacao;
+//        this.encomendas= new HashMap<>();
+//    }
+//    public HashMap getencomendas(){
+//        return encomendas;
+//    }
+//    public String verencomendas(){return encomendas.toString();}
+//
+//    // Gets e Setters
+//    public String getEstado() { return estado; }
+//    public void setEstado(String estado) { this.estado = estado; }
+//    public String getLocalizacao() { return Localizacao; }
+//    public void setLocalizacao(String localizacao) { Localizacao = localizacao; }
+//
+//
+//}
+>>>>>>> Stashed changes
