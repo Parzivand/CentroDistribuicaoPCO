e29f028eb689f2c2cf33b301f42ce598c4429d7e
Albanaz <miguelalbernaz2003@gmail.com>
2025-12-26 18:02:03 -0100

    completar o ultimo commit

--- CentroDistribuicaoPCO/src/java/handlers/ExpedicaoHandler.java
+++ CentroDistribuicaoPCO/src/java/handlers/ExpedicaoHandler.java
@@ -2,11 +2,15 @@ import java.domain.*;
 import java.util.*;
 public class ExpedicaoHandler{
     private ArrayList<Expedicao> expedicoes;
+
     public void associarEncomenda(Encomenda encomenda,Expedicao expedicao){
       if(encomenda!=null && !expedicao.getencomendas().containsKey(encomenda.getreferencia())){
         expedicao.getencomendas().put(encomenda.getreferencia(), encomenda);
-      } else if (!expedicoes.contains(expedicao)) {
-        
+      } else if (!expedicoes.contains(expedicao)|| encomenda==null) {
+        IO.println("a expedicao  nao  existe"); 
+
+      }else{
+        IO.print("erro");
       }
     }
 }
