27e5048fcd55a54f9114d1c5b5cb2ab3e66c769e
Parzivand <vdnbezerra@gmail.com>
2025-12-27 17:23:10 +0000

    fornecedor

--- CentroDistribuicaoPCO/src/codigo/domain/Encomenda.java
+++ CentroDistribuicaoPCO/src/codigo/domain/Encomenda.java
@@ -1,28 +1,28 @@
-//package java.domain;
-//
-//import java.util.ArrayList;
-//import java.util.HashMap;
-//
-//public class Encomenda {
-//    private  String estado,referencia;
-//    private Boolean prioridade;
-//    private final Loja loja;
-//    private ArrayList<LinhaEncomenda> linhasencomenda;
-//    public Encomenda(String estado,String referencia,Boolean prioridade, Loja loja ){
-//        this.estado= estado;
-//        this.referencia= referencia;
-//        this.prioridade=  prioridade;
-//        this.loja= loja;
-//
-//    }
-//    public String getestado(){return estado;}
-//    public void setestado(String  estado){this.estado = estado; }
-//    public String getreferencia(){return referencia;}
-//    public void setreferencia(String  referencia){this.referencia = referencia; }
-//    public String getloja(){return loja.toString();} // nao se vai mudar  a loja na encomenda ne ??
-//
-//    public String toString() {
-//        return String.format("estado:%s prioridade: %s",estado,prioridade,loja);
-//    }
-//
-//}
+package java.domain;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+
+public class Encomenda {
+    private  String estado,referencia;
+    private Boolean prioridade;
+    private final Loja loja;
+    private ArrayList<LinhaEncomenda> linhasencomenda;
+    public Encomenda(String estado,String referencia,Boolean prioridade, Loja loja ){
+        this.estado= estado;
+        this.referencia= referencia;
+        this.prioridade=  prioridade;
+        this.loja= loja;
+
+    }
+    public String getestado(){return estado;}
+    public void setestado(String  estado){this.estado = estado; }
+    public String getreferencia(){return referencia;}
+    public void setreferencia(String  referencia){this.referencia = referencia; }
+    public String getloja(){return loja.toString();} // nao se vai mudar  a loja na encomenda ne ??
+
+    public String toString() {
+        return String.format("estado:%s prioridade: %s",estado,prioridade,loja);
+    }
+
+}
--- CentroDistribuicaoPCO/src/codigo/domain/Fornecedor.java
+++ CentroDistribuicaoPCO/src/codigo/domain/Fornecedor.java
@@ -1,24 +1,53 @@
 package codigo.domain;
 
+import java.util.Objects;
+
 public class Fornecedor {
 
     // Atributos
-
-    private String nome, contacto;
+    private String nome;
+    private String email;
+    private String telefone;
 
     // Construtor
-
-    public Fornecedor(String nome, String contacto) {
+    public Fornecedor(String nome, String email, String telefone) {
         this.nome = nome;
-        this.contacto = contacto;
+        this.email = email;
+        this.telefone = telefone;
     }
 
-    // Gets e Setters
-
+    // Getters e Setters
     public String getNome() { return nome; }
     public void setNome(String nome) { this.nome = nome; }
 
-    public String getContacto() { return contacto; }
-    public void setContacto(String contacto) { this.contacto = contacto; }
+    public String getEmail() { return email; }
+    public void setEmail(String email) { this.email = email; }
 
-   }
+    public String getTelefone() { return telefone; }
+    public void setTelefone(String telefone) {
+        this.telefone = telefone;
+    }
+
+    // equals / hashCode (nome único, como pede UC03)
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Fornecedor)) return false;
+        Fornecedor that = (Fornecedor) o;
+        return Objects.equals(nome, that.nome);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(nome);
+    }
+
+    @Override
+    public String toString() {
+        return "Fornecedor{" +
+                "nome='" + nome + '\'' +
+                ", email='" + email + '\'' +
+                ", telefone='" + telefone + '\'' +
+                '}';
+    }
+}
