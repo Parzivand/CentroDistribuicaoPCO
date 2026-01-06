f164616f307b59fec47486adc3361fe86a6d6ade
Miguel Albernaz <miguelalbernaz2003@gmail.com>
2026-01-06 14:20:41 -0100

    atualizacao da lista de produtos para os fornecedores

--- CentroDistribuicaoPCO/src/codigo/handlers/FornecedorHandler.java
+++ CentroDistribuicaoPCO/src/codigo/handlers/FornecedorHandler.java
@@ -20,27 +20,67 @@ public class FornecedorHandler{
         String email,String telefone){
             if(!(nome==null||email==null||telefone==null)){
                 fornecedores.putIfAbsent(nome.trim().toUpperCase(),new Fornecedor(nome.trim(), email, telefone));
+                produtosdosfornecedores.put(new Fornecedor(nome.trim(), email, telefone),null);
                 // se ja existir o nome como chave ele nao adiciona ao map
             }else{
                 throw new IllegalArgumentException("falta elementos");            
             }
         }
-    // verifica se o fornecedor tem produtos UC20
+    // veifica se o fornecedor tem produtos UC20
+
+    public void associar_produtos(Produto produto,String fornecedorid){
+        if(produto==null|| fornecedorid==null||!fornecedores.containsKey(fornecedorid)){
+            throw new IllegalArgumentException("argumentos invalidos");
+        }
+        Set<Fornecedor> listafornecedores= produtosdosfornecedores.keySet();
+        for(Fornecedor fornecedor:listafornecedores){
+            if(fornecedor.getNome().equals(fornecedorid)&& produtosdosfornecedores.get(fornecedor).contains(produto)){
+                throw new IllegalArgumentException("o fornecedor ja tem  esse produto na lista");
+            }
+            if(fornecedor.getNome().equals(fornecedorid)&& !produtosdosfornecedores.get(fornecedor).contains(produto)){
+                produtosdosfornecedores.get(fornecedor).add(produto);
+            }
+            
+        }    
+    }
+    public void desassociarproduto(Produto produto,String fornecedorid){
+     if(produto==null|| fornecedorid==null||!fornecedores.containsKey(fornecedorid)){
+            throw new IllegalArgumentException("argumentos invalidos");
+        }
+        Set<Fornecedor> listafornecedores= produtosdosfornecedores.keySet();
+
+        for(Fornecedor fornecedor:listafornecedores){
+            if(fornecedor.getNome().equals(fornecedorid)&& produtosdosfornecedores.get(fornecedor).contains(produto)){
+                produtosdosfornecedores.get(fornecedor).remove(produto);
+            }
+            if(fornecedor.getNome().equals(fornecedorid)&& !produtosdosfornecedores.get(fornecedor).contains(produto)){
+                throw new IllegalArgumentException("o fornecedor nao  tem  produto na  sua  lista");
+            }
+            
+        }    
+    }   
+    
+
+
     public void removerfornecedor(String nome){
-        Set<Fornecedor> chaves = produtosdosfornecedores.keySet();// set com todos as chaves do mapa  produtosdosfornecedores para ser usado no loop    
+        Set<Fornecedor> listafornecedores= produtosdosfornecedores.keySet();// set com todos as chaves do mapa  
+        // produtosdosfornecedores para ser usado no loop    
         if(nome==null){
         }
         if(!fornecedores.containsKey(nome)){
         throw new IllegalArgumentException(String.format("o fornecedor com o nome %s nao esta registado",nome));
         }
-        for (Fornecedor fornecedor: chaves){
+        
+        for (Fornecedor fornecedor: listafornecedores){
             
-            if(fornecedor.getNome().equals(nome.trim())&& produtosdosfornecedores.get(fornecedor).isEmpty()
-            && fornecedores.containsKey(nome.trim())){// verifica se o fornecedor tem as  
+            if(fornecedor.getNome().equals(nome.trim())&& produtosdosfornecedores.get(fornecedor).isEmpty()){
+                // verifica se o fornecedor tem produtos na lista   
                 
                 fornecedores.remove(nome.trim());
                 produtosdosfornecedores.remove(fornecedor);
                 break;
+            }else{
+                IO.print("nao da para tirar esse fornecedor");
             }
         }
         
