89567edd9982f34c674da960cd2b6cece5bb20ab
Miguel Albernaz <miguelalbernaz2003@gmail.com>
2026-01-06 13:55:13 -0100

    crud_ fornecedor(D)

--- CentroDistribuicaoPCO/src/codigo/handlers/FornecedorHandler.java
+++ CentroDistribuicaoPCO/src/codigo/handlers/FornecedorHandler.java
@@ -6,6 +6,7 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.Scanner;
+import java.util.Set;
 import java.util.TreeMap;
 
 import codigo.domain.Fornecedor;
@@ -13,24 +14,48 @@ import codigo.domain.Produto;
 
 public class FornecedorHandler{
     Map<String,Fornecedor> fornecedores = new TreeMap<>();
+    Map<Fornecedor,ArrayList<Produto>> produtosdosfornecedores; //   fornecedores  com a sua lista de produtos 
     // UC03
     public void adicionarfornecedor(String nome,
         String email,String telefone){
             if(!(nome==null||email==null||telefone==null)){
-                fornecedores.putIfAbsent(nome.toUpperCase(),new Fornecedor(nome, email, telefone));
+                fornecedores.putIfAbsent(nome.trim().toUpperCase(),new Fornecedor(nome.trim(), email, telefone));
                 // se ja existir o nome como chave ele nao adiciona ao map
             }else{
                 throw new IllegalArgumentException("falta elementos");            
             }
         }
+    // verifica se o fornecedor tem produtos UC20
     public void removerfornecedor(String nome){
-        if(fornecedores.containsKey(nome.toUpperCase())){
-            fornecedores.remove(nome.toUpperCase());
-            // falta verificar se ele tem produtos associados 
+        Set<Fornecedor> chaves = produtosdosfornecedores.keySet();// set com todos as chaves do mapa  produtosdosfornecedores para ser usado no loop    
+        if(nome==null){
         }
+        if(!fornecedores.containsKey(nome)){
+        throw new IllegalArgumentException(String.format("o fornecedor com o nome %s nao esta registado",nome));
+        }
+        for (Fornecedor fornecedor: chaves){
+            
+            if(fornecedor.getNome().equals(nome.trim())&& produtosdosfornecedores.get(fornecedor).isEmpty()
+            && fornecedores.containsKey(nome.trim())){// verifica se o fornecedor tem as  
+                
+                fornecedores.remove(nome.trim());
+                produtosdosfornecedores.remove(fornecedor);
+                break;
+            }
+        }
+        
+    }
+    
+
+    // mostra o fornecedor que tem aquele nome 
+    public Fornecedor verpornome(String nome){
+        if(!fornecedores.containsKey(nome.trim().toUpperCase())){
+            throw new IllegalArgumentException("Esse fornecedor nao existe");
+        }
+        return fornecedores.get(nome.trim().toUpperCase());
     }
 
-  public ArrayList<Produto>  getProdutos(int valor) { 
+  public ArrayList<Fornecedor>  getfornecedores(int valor) { 
    // criei uma lista com todos os valores do Map para que se possa  mostrar todos os valores de 10 em 10 caso
    // seja solicitado que sera  configurado  no  menu 
    
